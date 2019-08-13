6c7a0ec3ec7c76b727d6324cf7ede527436f7ae7
Severin Klaus <sk@hinderlingvolkart.com>
2019-08-13 18:16:56 +0200

    Tag as 2-10

--- noderuby-build/build-docker.sh
+++ noderuby-build/build-docker.sh
@@ -2,5 +2,5 @@
 mkdir -p base/bin
 find ../shared-bin -type f -not -iname '*.sh' -exec cp {} base/bin/ \;
 
-docker build -t hinderlingvolkart/noderuby-build -t hinderlingvolkart/noderuby-build:latest -t hinderlingvolkart/noderuby-build:2-8 ./base
+docker build -t hinderlingvolkart/noderuby-build:2-10 ./base
 docker push hinderlingvolkart/noderuby-build

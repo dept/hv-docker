d07966326cb34a96ff139063a7a75abcd0851b17
Dario Merz <dme@hinderlingvolkart.com>
2020-05-26 11:46:19 +0200

    chore: adds non-alpine versions aswell

--- node-build/build-docker.sh
+++ node-build/build-docker.sh
@@ -3,5 +3,5 @@ mkdir -p base/bin
 find ../shared-bin -type f -not -iname '*.sh' -exec cp {} base/bin/ \;
 
 
-docker build -t hinderlingvolkart/node-build -t hinderlingvolkart/node-build:current-alpine ./base
+docker build -t hinderlingvolkart/node-build -t hinderlingvolkart/node-build:current ./base
 docker push hinderlingvolkart/node-build

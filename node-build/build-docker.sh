d0b91c1518d83026b89f93898f6fbcaf10265380
Severin Klaus <sk@hinderlingvolkart.com>
2020-10-01 17:47:51 +0200

    Updates docker for ruby

--- node-build/build-docker.sh
+++ node-build/build-docker.sh
@@ -3,5 +3,5 @@ mkdir -p base/bin
 find ../shared-bin -type f -not -iname '*.sh' -exec cp {} base/bin/ \;
 
 
-docker build -t hinderlingvolkart/node-build -t hinderlingvolkart/node-build:current ./base
+docker build -t hinderlingvolkart/node-build:14 ./base
 docker push hinderlingvolkart/node-build
